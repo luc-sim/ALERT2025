425eaab17126b74c07791247c61cc3419b267e84
luc-sim <58327465+luc-sim@users.noreply.github.com>
2025-10-01 17:29:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ if st.button("Calculate"):
     t = min( (6.35/1000 + D/100), 0.09 )    # [m] wall thickness
     I       = np.pi/4 * ((D/2)**4 - (D/2-t)**4) # Second moment of area
     EI      = E * I
-    fig, axes = plt.subplots(1,4,figsize=(16,8))
+    fig, axes = plt.subplots(2,2,figsize=(16,16))
     fig.suptitle('For D={0:.2f} m, L={1:.2f} m and t={2:.0f} mm, we get y(0)/D={3:.3f}, a bending stress safety factor {5:.2f}, and Steel amount {4:.0f} t\n'\
             .format(D,L,t*1000,y[2]/D,W,355*10**6/(max(np.abs(M))*D/2/I))+\
         'The theoretical profile is pictured in dashed black for kh={0:.1f} MN'\
@@ -360,6 +360,7 @@ if st.button("Calculate"):
 
     st.write(y)
     
+
 
 
 
